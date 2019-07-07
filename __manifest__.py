7d1e279806acaeae087b988452df9f8b260ae031
unostore <jho3893@mail.kvcc.edu>
2019-07-06 19:35:16 -0700

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,16 +2,16 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
     'license': 'LGPL-3',
-    "name" : "Odoo Mobile App buider",
+    "name" : "TeaPay Connector",
     "version" : "1.0",
-    "author" : "saas tools",
-    'price': 299.90,
-    'currency': 'EUR',
+    "author" : "TeaPay",
+    
+    'currency': 'US',
     "category" : "Sales",
-	"summary": "Personalize your Own Odoo Mobile App with odoo app builer!",
-    'description': "Odoo is the greatest web app system for enterprise but doesn't have app for community version, besides on that W360S Team we build on react native that can help you to generate your own Odoo system on mobile for both android & ios. So now you can have your own mobile apps, just need Odoo CE + Odoo backend theme and with Odoo mobile app builder you can get your own system with your own brand name.",
-    'maintainer': "World 360° Services",
-    'website': 'https://www.saastools.xyz',
+	
+   
+    'maintainer': "TeaPay",
+    'website': 'https://www.teapay.io',
     'images': ['static/description/banner.jpg'],
     "depends" : ['base',
                 ],
